ea4c270e2396a04c3af0730a6ecec2ae07c8ad1a
Will <doggy.huang@gmail.com>
2016-08-13 11:04:39 +0800

    06 練習 Angular 2 繫結方法：屬性繫結 ( Property Binding )

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   title = 'The Will Will Web !!';
+  titleurl = 'http://blog.miniasp.com/';
+
+  subtitle = '記載著 <strong>Will</strong> 在網路世界的學習心得與技術分享';
 
   constructor() { }
 
