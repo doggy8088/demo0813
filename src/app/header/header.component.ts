43adce41c0c864be047223dc6efa3ed6542c5cd9
Will <doggy.huang@gmail.com>
2016-08-13 14:46:34 +0800

    套用 encapsulation: ViewEncapsulation.Emulated 控制樣式的封裝特性

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: 'header.component.html',
-  styleUrls: ['header.component.css']
+  styleUrls: ['header.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class HeaderComponent implements OnInit {
 
